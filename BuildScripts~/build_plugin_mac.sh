f33ff2bbbec57ddb01571013b9dd212a215210f8
Andrzej Kapolka <andrzej.kapolka@unity3d.com>
2025-10-31 11:21:16 -0700

    Update plugin build script, macOS plugin dylib.

--- BuildScripts~/build_plugin_mac.sh
+++ BuildScripts~/build_plugin_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M116/webrtc-mac.zip
+export ARTIFACTS_DIR="$(pwd)/artifacts"
 export SOLUTION_DIR=$(pwd)/Plugin~
 export DYLIB_FILE=$(pwd)/Runtime/Plugins/macOS/libwebrtc.dylib
 
@@ -8,14 +9,14 @@ export DYLIB_FILE=$(pwd)/Runtime/Plugins/macOS/libwebrtc.dylib
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew install cmake
 
-# Download LibWebRTC 
-curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
-unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
+# Download LibWebRTC
+#curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
+unzip -o -d $SOLUTION_DIR/webrtc "$ARTIFACTS_DIR/webrtc-mac.zip"
 
 # Remove old dylib file
 rm -rf "$DYLIB_FILE"
 
 # Build UnityRenderStreaming Plugin
 cd "$SOLUTION_DIR"
-cmake --preset=macos
+cmake --preset=macos -D CMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build --preset=release-macos --target=WebRTCPlugin
